d2e804f42585f63d929f980ab647ae3b5028c365
dmlob <dmlob@DESKTOP-0VEH751>
2023-02-18 22:56:44 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIDuctCreate
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ MainViewViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPILibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIDuctCreate
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public List<MEPCurveType> DuctTypes { get; } = new List<MEPCurveType>();
+
+        public List<Level> Levels { get; } = new List<Level>();
+
+        public DelegateCommand SaveCommand { get; }
+
+        public double WallHeight { get; set; }
+
+        public MEPCurveType SelectedDuctType { get; set; }
+
+        public Level SelectedLevel { get; set; }
+
+        public List<XYZ> Points { get; } = new List<XYZ>();
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            DuctTypes = DuctUtils.GetMEPSystemType(commandData);
+            Levels = LevelsUtils.GetLevels(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            WallHeight = 100;
+            Points = SelectionUtils.GetPoints(_commandData, "Выберите точки", ObjectSnapTypes.Endpoints);
+        }
+
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            MEPSystemType systemType = new FilteredElementCollector(doc)
+                .OfClass(typeof(MEPSystemType))
+                .Cast<MEPSystemType>()
+                .FirstOrDefault(m => m.SystemClassification == MEPSystemClassification.SupplyAir);
+
+            if (Points.Count < 2 ||
+              SelectedDuctType == null ||
+              SelectedLevel == null)
+                return;
+
+            using (var ts = new Transaction(doc, "Create duct"))
+            {
+                ts.Start();
+                Duct.Create(doc, systemType.Id, SelectedDuctType.Id, SelectedLevel.Id, Points[0], Points[1]);
+                ts.Commit();
+            }
+        }
+
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
